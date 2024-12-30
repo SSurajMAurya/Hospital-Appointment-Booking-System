dce0abf213eeaba00a347c3184a9b397d546a37e
Suraj <surajmaurya15102000@gmail.com>
2024-12-30 17:55:35 +0530

    Initial commit

---
+++ src/main/java/com/HAB/main/HospitalAppointmentBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.HAB.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalAppointmentBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalAppointmentBookingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/HAB/main/HospitalAppointmentBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HAB.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalAppointmentBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
